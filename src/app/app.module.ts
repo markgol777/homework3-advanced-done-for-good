872775c91b7ca1c710c96756b47389c4412b28e2
markgol777 <markgol777@gmail.com>
2021-12-16 17:37:24 +0200

    added all files

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponentComponent } from './main-component/main-component.component';
+import { ChildComponent } from './main-component/child/child.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponentComponent,
+    ChildComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
